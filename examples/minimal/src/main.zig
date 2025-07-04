1bad2f23773392ba92b3eb1e2cffbedfd299adab
xanners <xxdr@duck.com>
2025-07-03 19:40:03 -0500

    refactor(examples/minimal): change size hint to `resizable` [skip ci]

--- examples/minimal/src/main.zig
+++ examples/minimal/src/main.zig
@@ -5,7 +5,7 @@ pub fn main() !void {
     const config = libquark.WindowConfig.init()
         .with_title("Quark - minimal")
         .with_dimensions(1280, 720)
-        .with_size_hint(libquark.WindowHint.min_size)
+        .with_size_hint(libquark.WindowHint.resizable)
         .with_debug(true);
 
     const window = try libquark.create_window(config);
